0c0cbcea5f9315223a3703c52f657cf9f16eabcf
Josh Uzo <nwankwo.joshua@gmail.com>
2019-12-16 17:01:14 +1100

    Test case refactoring

--- src/test/java/au/com/mebank/codingchallenge/joshluisaac/transactionprocessing/RelativeAccountBalanceTest.java
+++ src/test/java/au/com/mebank/codingchallenge/joshluisaac/transactionprocessing/RelativeAccountBalanceTest.java
@@ -2,15 +2,19 @@ package au.com.mebank.codingchallenge.joshluisaac.transactionprocessing;
 
 import static org.assertj.core.api.Assertions.*;
 
+import au.com.mebank.codingchallenge.joshluisaac.AbstractTest;
 import au.com.mebank.codingchallenge.joshluisaac.FakeTestData;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class RelativeAccountBalanceTest {
+public class RelativeAccountBalanceTest extends AbstractTest {
 
   private RelativeAccountBalance relativeAccountBalance;
+  private Transactions transactions;
   private static List<Transaction> dataSet;
 
   @BeforeEach
@@ -23,7 +27,7 @@ public class RelativeAccountBalanceTest {
     TransactionQueryScope queryScope =
         TransactionQueryScope.builder().accountId("ACC334455").timeFrame(timeFrame).build();
     dataSet = FakeTestData.fakeDataSet();
-    ITransactions<Transaction> transactions = new Transactions(dataSet, queryScope);
+    transactions = new Transactions(dataSet, queryScope);
     relativeAccountBalance = new RelativeAccountBalance(transactions);
   }
 
@@ -40,19 +44,46 @@ public class RelativeAccountBalanceTest {
   }
 
   @Test
+  void shouldEvaluate_PaymentAndReversalTransactions() {
+
+    List<Transaction> data = new ArrayList<>();
+    LocalDateTime transactionDate = TransactionUtils.parseDate("20/10/2018 14:00:01");
+    LocalDateTime reversalTransactionDate = TransactionUtils.parseDate("27/10/2018 09:10:55");
+
+    // when i initiate 3 payments
+    data.add(
+        makePayment("TX80003", "ACC334455", "ACC778899", transactionDate, new BigDecimal("10.00")));
+    data.add(
+        makePayment("TX80004", "ACC334455", "ACC778899", transactionDate, new BigDecimal("5.50")));
+    data.add(
+        makePayment("TX80005", "ACC334455", "ACC778899", transactionDate, new BigDecimal("25.01")));
+
+    // and 1 reversal
+    data.add(
+        reversePayment(
+            "TX80006",
+            "ACC334455",
+            "ACC778899",
+            reversalTransactionDate,
+            new BigDecimal("5.50"),
+            "TX80004"));
+
+    transactions.setTransactionDataSet(data);
+    Result result = relativeAccountBalance.balance();
+    int compareBalance = (result.getBalance().compareTo(new BigDecimal("-35.01")));
+    boolean actualResult = compareBalance == 0 && (result.getTransactionsIncluded() == 2);
+    assertThat(actualResult).isTrue();
+  }
+
+  @Test
   void shouldIncrementRelativeBalance_OnCreditTransactionEntry() {
 
     // when a new credit transaction is added within the time frame
-    Transaction creditTrx =
-        Transaction.builder()
-            .transactionId("TX80003")
-            .fromAccountId("ACC778899")
-            .toAccountId("ACC334455")
-            .createdAt(TransactionUtils.parseDate("20/10/2018 14:00:01"))
-            .amount(new BigDecimal("42.00"))
-            .transactionType(TransactionType.PAYMENT)
-            .build();
-    dataSet.add(creditTrx);
+    LocalDateTime transactionDate = TransactionUtils.parseDate("20/10/2018 14:00:01");
+    dataSet.add(
+        makePayment("TX80003", "ACC778899", "ACC334455", transactionDate, new BigDecimal("42.00")));
+
+    // then it should increment relative balance
     Result result = relativeAccountBalance.balance();
     assertThat(result.getBalance()).isEqualTo(new BigDecimal("17.00"));
   }
@@ -61,16 +92,11 @@ public class RelativeAccountBalanceTest {
   void shouldIncrementTransactionsIncluded_OnCreditTransactionEntry() {
 
     // when a new credit transaction is added within the time frame
-    Transaction creditTrx =
-        Transaction.builder()
-            .transactionId("TX80003")
-            .fromAccountId("ACC778899")
-            .toAccountId("ACC334455")
-            .createdAt(TransactionUtils.parseDate("20/10/2018 14:00:01"))
-            .amount(new BigDecimal("42.00"))
-            .transactionType(TransactionType.PAYMENT)
-            .build();
-    dataSet.add(creditTrx);
+    LocalDateTime transactionDate = TransactionUtils.parseDate("20/10/2018 14:00:01");
+    dataSet.add(
+        makePayment("TX80003", "ACC778899", "ACC334455", transactionDate, new BigDecimal("42.00")));
+
+    // then it should increase the transactions included
     Result result = relativeAccountBalance.balance();
     assertThat(result.getTransactionsIncluded()).isEqualTo(2);
   }
